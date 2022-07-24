5f402915eb70923f479f03ba57989e5c46f3cd39
VitorDanielAdams <vitor.adams@outlook.com>
2022-07-24 16:09:59 -0300

    fix: Arrumando Responsividade

--- src/components/FolhaA4/A4Styles.ts
+++ src/components/FolhaA4/A4Styles.ts
@@ -2,9 +2,13 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
     width: 794px;
-    height: 1123px;
+    height: 1100px;
     background-color: #FFFFFF;
     box-shadow: 0 0 10px rgb(153, 153, 153);
     padding: 10px;
     position: relative;
+    @media only screen and (min-width: 768px) {
+        width: 794px;
+        height: 1123px;
+    }
 `;
--- src/components/Grafico/GraficoStyles.ts
+++ src/components/Grafico/GraficoStyles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import ReactECharts from 'echarts-for-react'; 
 
 function calculateResposive(value: string){
-    return  ~~value * 60 / 100;
+    return  ~~value * 70 / 100;
 }
 
 export const ECharts = styled(ReactECharts)<{width:string, height:string}>`
